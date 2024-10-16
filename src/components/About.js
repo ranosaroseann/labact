76bb2c5827862b530681b7d904e453e9b97a15eb
ranosaroseann <ranosaroseann08@gmail.com>
2024-10-17 00:37:03 +0800

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Style.css';
+
+const About = () => {
+  return (
+    <div>
+      <h1>ABOUT ME</h1>
+      <p>Im currently studying at Pamantasan ng Cabuyao.</p>
+      <p>Im a working student and currently working at JB Pulo.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Style.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <header className="header">
+                <h1>WELCOME TO MY PROFILE</h1>
+            </header>
+            <main className="main-content">
+                <p>“Find out what you like doing best, and get someone to pay you for doing it.”</p>
+            </main>
+            
+        </div>
+    );
+};
+
+export default Home;
